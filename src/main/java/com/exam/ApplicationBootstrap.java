3b029ae55b958ae3d686dc0b74a416c274797862
Mehul <mehul8055@gmail.com>
2018-09-11 22:11:37 +0530

    modified jar to war

--- src/main/java/com/exam/ApplicationBootstrap.java
+++ src/main/java/com/exam/ApplicationBootstrap.java
@@ -2,10 +2,17 @@ package com.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationBootstrap {
+public class ApplicationBootstrap extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationBootstrap.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationBootstrap.class, args);
 	}
